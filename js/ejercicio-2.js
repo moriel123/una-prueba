8858f7691a41d686b194299c5522cdf5a419b45a
moriel123 <karinmoriel.low@alumnos.ulagos.cl>
2024-06-24 20:57:55 -0400

    tarea

---
+++ js/ejercicio-2.js
@@ -0,0 +1,53 @@
+//ejercicios de array
+//Declarar un array de numeros
+
+let numero=[1,2,3,4,5];
+console.log(numero);
+
+//Acceder a elementos de un array
+let frutas=['careza','durazno','platano','frutilla'];
+console.log(frutas[1]);
+
+//Modificar un elemento de un array
+ let colores=['morado','rosado','azul'];
+ colores[0]='verde';
+ console.log(colores);
+
+ //Añadir un elemento a un array
+let animales=['oso','lobo','leon'];
+animales[3]='pajaro';
+console.log(animales);
+
+//ejercicios con objetos
+//Declarar un objeto simple
+let persona={
+ nombre:"Moriel",
+ edad:22,
+ ciudad:"Quellon",
+
+}
+console.log(persona);
+
+//acceder a una propiedad de un objeto
+let coche={
+ marca:"chevrolet",
+ modelo:"cheyenne",
+ año:2015,
+}
+console.log(coche.modelo);
+
+//Modificar una propiedad de un objeto
+let libro={
+ titulo:"rebelion de la granja",
+ autor:"Pepe grillo",
+}
+libro.autor="George Orwell";
+console.log(libro);
+
+//añadir una nueva propiedad a un objeto
+let pelicula={
+ titulo:"la mascara",
+ director:"Chuck Russell",
+}
+pelicula.año="1994";
+console.log(pelicula);
---
+++ js/index.js
@@ -0,0 +1,27 @@
+// ejercicio 1 : variables y asignacion
+let myname="Moriel";
+console.log(myname);
+let edad="22 años";
+console.log(edad);
+
+//ejercicio 2:Tipos de datos
+let numero=21;
+let cadena="hola";
+let booleano=true;
+
+console.log(typeof numero);
+console.log(typeof cadena);
+console.log(typeof booleano);
+
+//ejercicio 3: constantes
+
+const PI=3.14159;
+console.log(PI);
+
+//ejercico 4: comentarios
+
+//hola
+/* ho
+la
+ */
+
